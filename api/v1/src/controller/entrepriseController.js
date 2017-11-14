88d908813d66f530da97110228776e3d78d3d8da
snipzer <l.arif@laposte.net>
2017-11-14 18:00:01 +0100

    #22 Creation du service de gestion des codes d'erreurs

--- api/v1/src/controller/entrepriseController.js
+++ api/v1/src/controller/entrepriseController.js
@@ -1,14 +1,12 @@
 import EntrepriseHandler from "../models/entrepriseHandler";
 import _ from "underscore";
-import eV from "../models/entrepriseValidator";
+import httpStatusService from "../services/httpStatusService";
 
 export default class EntrepriseController
 {
 
     constructor() {
         this.entrepriseHandler = new EntrepriseHandler();
-        this.eV = new eV();
-        this.status = this.eV.getStatus();
     }
 
     index(req, res) {
@@ -17,33 +15,39 @@ export default class EntrepriseController
 
     getEntreprises(req, res) {
         this.entrepriseHandler.getEntreprises()
-            .then(entreprise => res.json(entreprise))
-            .catch(err => res.json(err))
+            .then(entreprise =>  this.sendJsonResponse(httpStatusService.ok, entreprise))
+            .catch(err => this.sendJsonResponse(httpStatusService.internalServerError, err))
     }
 
     getEntrepriseById(req, res) {
         const id = req.params.id;
 
         this.entrepriseHandler.getEntreprise(id)
-            .then(entreprise => res.json(entreprise))
-            .catch(err => res.json(err))
+            .then(entreprise =>  this.sendJsonResponse(httpStatusService.ok, entreprise))
+            .catch(err => this.sendJsonResponse(httpStatusService.internalServerError, err))
     }
 
     putEntreprises(req, res) {
-        const param = req.body;
         const id = req.params.id;
-
-        let array = {
-            label: (!_.isNull(param.label)) ? param.label : null,
-            email: (!_.isNull(param.email)) ? param.email : null,
-            password: (!_.isNull(param.password)) ? param.password : null,
-            url_ad: (!_.isNull(param.url_ad)) ? param.url_ad : null,
-            url_picture: (!_.isNull(param.url_picture)) ? param.url_picture : null,
-            campaign: (!_.isNull(param.campaign)) ? param.campaign : null
-        };
-
+        let array = this.setArrayFromBody(req.body);
         this.entrepriseHandler.putEntreprise(id, array)
-            .then(entreprise => res.json(entreprise))
-            .catch(err => res.json(err));
+            .then(entreprise => this.sendJsonResponse(httpStatusService.ok, entreprise))
+            .catch(err => this.sendJsonResponse(httpStatusService.internalServerError, err));
+    }
+
+    private sendJsonResponse(code, content) {
+        res.status(code);
+        res.json(content);
+    }
+
+    private static setArrayFromBody(body) {
+        let array = {};
+        array.label = (!_.isNull(body.label)) ? body.label : null;
+        array.email = (!_.isNull(body.email)) ? body.email : null;
+        array.password = (!_.isNull(body.password)) ? body.password : null;
+        array.url_ad = (!_.isNull(body.url_ad)) ? body.url_ad : null;
+        array.url_picture = (!_.isNull(body.url_picture)) ? body.url_picture : null;
+        array.campaign = (!_.isNull(body.campaign)) ? body.campaign : null;
+        return array
     }
 }
---
+++ api/v1/src/services/httpStatusService.js
@@ -0,0 +1,12 @@
+export default class HttpStatusService
+{
+    public static ok = 200;
+    public static created = 201;
+    public static noContent = 204;
+    public static badRequest = 400;
+    public static unauthorized = 401;
+    public static forbidden = 403;
+    public static notFound = 404;
+    public static methodNotAllowed = 405;
+    public static internalServerError = 500;
+}
