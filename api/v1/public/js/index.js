f21d5b125bfca9663bdbcb1df44e14f8b6544727
Benoît Ripoche <ripoche.b@gmail.com>
2017-11-15 16:16:42 +0100

    #13 je vous previens, ça va saigner

---
+++ api/v1/public/js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    var token = getToken();
+
+    if (!token) {
+        $("#entreprise").hide();
+        $(".login-card").show();
+    } else {
+        listCampaigns();
+    }
+
+
+    $("#submit").click(function(e)
+    {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/v1/login',
+            type: 'POST',
+            data: { email: $("#email").val(), password: $("#password").val() },
+            dataType: 'json',
+            success: function(data, status)
+            {
+                localStorage.setItem("api_token", data.token);
+                listCampaigns();
+            },
+            error: function(result, status, error)
+            {
+                console.dir(error);
+            }
+        });
+    });
+
+    function listCampaigns() {
+        var token = getToken();
+
+        if (!token) {
+           //todo gestion de l'erreur
+        }
+        else {
+            $.ajax({
+                url: '/mock/campaigns',
+                type: 'GET',
+                headers: {"Authorization": token},
+                dataType: 'json',
+                success: function(data, status)
+                {
+                    $("#entreprise").show();
+                    $(".login-card").hide();
+                },
+                error: function(result, status, error)
+                {
+                    console.dir(error);
+                }
+            });
+        }
+    }
+
+    function getToken() {
+        var token = localStorage.getItem('api_token');
+
+        if(token == null || typeof token == "undefined") {
+            return false
+        }
+        return token;
+    }
+
+})
