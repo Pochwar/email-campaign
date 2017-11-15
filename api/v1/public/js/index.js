37bf8c014fa37ee7784804aa308c744afe833b89
snipzer <l.arif@laposte.net>
2017-11-15 16:35:44 +0100

    Merge de register avec index

--- api/v1/public/js/index.js
+++ api/v1/public/js/index.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
     var token = getToken();
 
     if (!token) {
+        $('#register-card').hide();
         $("#entreprise").hide();
         $("#login").show();
     } else {
         listCampaigns();
     }
 
+    $('#show-register').unbind('click').bind('click', function () {
+        $('#login-card').hide();
+        $('#register-card').show();
+        handleRegister();
+    });
 
     $("#submit").click(function(e)
     {
@@ -63,4 +69,47 @@ $(document).ready(function() {
         return token;
     }
 
-})
+    function handleRegister() {
+        var token = localStorage.getItem('api_token');
+
+        if(token == null || typeof token == "undefined") {
+            $('#register-card').hide();
+            $('#login-card').show();
+        } else {
+            $("#submit-register").unbind('click').bind('click', function () {
+                var label = $('#label').val();
+                var email = $('#email').val();
+                var password = $('#password').val();
+                var passwordConfirm = $('#password-confirm').val();
+                var url_ad = $('#url-ad').val();
+                var url_picture = $('#url-picture').val();
+
+                if(password === passwordConfirm)
+                {
+                    $.ajax({
+                        url: "/api/v1/entreprises",
+                        headers: {"Authorization": token},
+                        method: "POST",
+                        data: {
+                            label: label,
+                            email: email,
+                            password: password,
+                            url_ad: url_ad,
+                            url_picture: url_picture
+                        },
+                        success: function () {
+                            $('#register-card').hide();
+                            $('#login-card').show();
+                        },
+                        error: function (error) {
+                            $('.alert-error').html(error.message);
+                        }
+                    })
+                } else {
+                    $('.alert-error').html("Erreur les mots de passes ne corresponde pas");
+                }
+            });
+        }
+    }
+
+});
