a7646791e72231d5b8aa28da1d53ac479aed2fa5
Benoît Ripoche <ripoche.b@gmail.com>
2017-11-15 10:44:15 +0100

    add fake campaigns

--- api/v1/src/services/mockingService.js
+++ api/v1/src/services/mockingService.js
@@ -1,5 +1,3 @@
-import faker from 'faker';
-
 export default class MockingService
 {
     constructor(){
@@ -7,16 +5,38 @@ export default class MockingService
     }
 
     generateCampaigns(req, res){
-        // todo generate campaigns data
-        const data = [];
-        for(let i=0; i < 10; i++){
-            const randomName = faker.name.findName();
-            const randomEmail = faker.internet.email();
-            data.push({
-                name: "tavu",
-                email: randomEmail
-            })
-        }
+        const data = [
+            {
+                id: "123456",
+                contenu: "blabla ceci est le contenu de la campagne N° 123456",
+                date: "04-12-2017",
+                sujet: "Sujet de la campagne N° 123456"
+            },
+            {
+                id: "456789",
+                contenu: "blabla ceci est le contenu de la campagne N° 456789",
+                date: "05-12-2017",
+                sujet: "Sujet de la campagne N° 456789"
+            },
+            {
+                id: "000000",
+                contenu: "blabla ceci est le contenu de la campagne N° 000000",
+                date: "06-12-2017",
+                sujet: "Sujet de la campagne N° 000000"
+            },
+            {
+                id: "99999",
+                contenu: "blabla ceci est le contenu de la campagne N° 99999",
+                date: "07-12-2017",
+                sujet: "Sujet de la campagne N° 99999"
+            },
+            {
+                id: "123123",
+                contenu: "blabla ceci est le contenu de la campagne N° 123123",
+                date: "08-12-2017",
+                sujet: "Sujet de la campagne N° 123123"
+            },
+        ];
         res.json({data: data});
     }
 }
