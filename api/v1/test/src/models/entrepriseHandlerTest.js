5c3d95345856b2064d5bac8cc9137b08e373dc08
snipzer <l.arif@laposte.net>
2017-11-15 14:13:44 +0100

    #42 Ajout de dummy test

---
+++ api/v1/test/src/models/entrepriseHandlerTest.js
@@ -0,0 +1,35 @@
+const expect = require('expect');
+
+
+describe("EntrepriseHandler", () => {
+
+    describe("#getEntreprises", () => {
+        it("Devrait renvoyer la listes des entreprises", () => {
+            expect(true).toBe(true);
+        });
+    });
+
+    describe("#getEntrepriseById", () => {
+        it("Devrait renvoyer l'entreprise correspondant à l'id", () => {
+            expect(true).toBe(true);
+        })
+    });
+
+    describe("#postEntreprise", () => {
+        it("Devrait renvoyer l'entreprise créer", () => {
+            expect(true).toBe(true);
+        })
+    });
+
+    describe("#putEntreprise", () => {
+        it("Devrait modifier les informations de l'entreprise", () => {
+            expect(true).toBe(true);
+        })
+    });
+
+    describe("#deleteEntreprise", () => {
+        it("Devrait renvoyer un json confirmant la suppresion si l'id existe", () => {
+            expect(true).toBe(true);
+        })
+    })
+});
