336a42fc5be04abbb58db58bd41d23381ebc6d56
snipzer <l.arif@laposte.net>
2017-11-15 15:23:18 +0100

    #33 #34 Ajout de la page register

--- api/v1/public/js/register.js
+++ api/v1/public/js/register.js
@@ -1,34 +1,42 @@
 $(document).ready(function()
 {
-    $("#submit").unbind('click').bind('click', function () {
-        var label = $('#label').val();
-        var email = $('#email').val();
-        var password = $('#password').val();
-        var passwordConfirm = $('#password-confirm').val();
-        var url_ad = $('#url-ad').val();
-        var url_picture = $('#url-picture').val();
+    var token = localStorage.getItem('api_token');
 
-        if(password === passwordConfirm)
-        {
-            $.ajax({
-                url: "/api/v1/entreprises",
-                method: "POST",
-                data: {
-                    label: label,
-                    email: email,
-                    password: password,
-                    url_ad: url_ad,
-                    url_picture: url_picture
-                },
-                success: function (data) {
-                    console.log("ok");
-                    console.log(data);
-                },
-                error: function (error) {
-                    console.log('pas ok');
-                    console.log(error)
-                }
-            })
-        }
-    });
+    if(token == null || typeof token == "undefined") {
+        $('.register-card').hide();
+        $('.login-card').show();
+    } else {
+        $("#submit").unbind('click').bind('click', function () {
+            var label = $('#label').val();
+            var email = $('#email').val();
+            var password = $('#password').val();
+            var passwordConfirm = $('#password-confirm').val();
+            var url_ad = $('#url-ad').val();
+            var url_picture = $('#url-picture').val();
+
+            if(password === passwordConfirm)
+            {
+                $.ajax({
+                    url: "/api/v1/entreprises",
+                    headers: {"Authorization": token},
+                    method: "POST",
+                    data: {
+                        label: label,
+                        email: email,
+                        password: password,
+                        url_ad: url_ad,
+                        url_picture: url_picture
+                    },
+                    success: function () {
+                        $('.register-card').hide();
+                        $('.login-card').show();
+                    },
+                    error: function (error) {
+                        console.log('pas ok');
+                        console.log(error)
+                    }
+                })
+            }
+        });
+    }
 });
