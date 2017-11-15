fe5e6b782d286bbaa5408796f2d24c51e9630895
snipzer <l.arif@laposte.net>
2017-11-15 14:50:09 +0100

    #33 #34 toujours en cours

--- api/v1/public/js/register.js
+++ api/v1/public/js/register.js
@@ -5,12 +5,30 @@ $(document).ready(function()
         var email = $('#email').val();
         var password = $('#password').val();
         var passwordConfirm = $('#password-confirm').val();
-        var urlAd = $('#url-ad').val();
-        var urlPicture = $('#url-picture').val();
+        var url_ad = $('#url-ad').val();
+        var url_picture = $('#url-picture').val();
 
         if(password === passwordConfirm)
         {
-
+            $.ajax({
+                url: "/api/v1/entreprises",
+                method: "POST",
+                data: {
+                    label: label,
+                    email: email,
+                    password: password,
+                    url_ad: url_ad,
+                    url_picture: url_picture
+                },
+                success: function (data) {
+                    console.log("ok");
+                    console.log(data);
+                },
+                error: function (error) {
+                    console.log('pas ok');
+                    console.log(error)
+                }
+            })
         }
     });
 });
