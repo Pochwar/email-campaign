10bdfe43d004d3755e1544109a39cc4d7e563a5b
Benoît Ripoche <ripoche.b@gmail.com>
2017-11-16 16:52:08 +0100

    deploy

--- src/models/mongooseHandler.js
+++ src/models/mongooseHandler.js
@@ -8,7 +8,7 @@ export default class mongooseHandler
         this.mongoose = Mongoose;
 
         let dbConnect;
-        if(process.env.ENV == 'prod') {
+        if(process.env.ENV == 'production') {
             dbConnect = `mongodb://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`
         } else {
             dbConnect = `mongodb://${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`
